60897a977abe08a5d97ddf5a7b47720c24afc5fd
Nikita <fedorovni1996@yandex.ru>
2019-08-22 19:37:02 +0500

    Добавил перемещение svg вне спрайта в build

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,11 +1,11 @@
 module.exports = function() {
     $.gulp.task('img:dev', function () {
-        return $.gulp.src('src/static/img/**/*.{png,jpg}')
+        return $.gulp.src('src/static/img/*.{png,jpg,svg}')
             .pipe($.gulp.dest('build/static/img'));
     });
 
     $.gulp.task('img:build', function () {
-        return $.gulp.src('src/static/img/**/*.{png,jpg}')
+        return $.gulp.src('src/static/img/*.{png,jpg,svg}')
             .pipe($.imagemin([
                 $.mozjpeg({
                 quality: 65
